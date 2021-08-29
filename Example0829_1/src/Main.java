4c006d01d859e6fb57492a4d8f2bbf8fb51f1b32
DaeYong_0412 <rex041200@naver.com>
2021-08-29 22:56:06 +0900

    자바 강의 수강 두 번째

---
+++ Example0829_1/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.print("Hello ");
+		System.out.print("World~");
+		
+		System.out.println("Hello ");
+		System.out.println("World~");
+	}
+
+}
