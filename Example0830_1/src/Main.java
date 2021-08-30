23a3c1c19922c199d3edb1c3cb2ed2b3ffc2de35
DaeYong_0412 <rex041200@naver.com>
2021-08-30 16:42:24 +0900

    자바 강의 수강 세 번째

---
+++ Example0830_1/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int level = 0;
+		//System.out.println(level);
+		
+		level = 10;
+		System.out.println(level + 5);
+		
+		double pi = 3.141592;
+		System.out.println(level + pi);
+		
+		String myName = "À±ÇÊ»ó";
+		System.out.println(level + myName);
+	}
+
+}
