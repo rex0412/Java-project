009906a593c456a7f27930636141e532501e9afb
DaeYong_0412 <rex041200@naver.com>
2021-08-31 21:41:42 +0900

    상수와 리터럴, 형변환

---
+++ Example0831_1/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		final double PI = 3.14;
+		int radius = 5;
+		final String MY_NAME = "윤필상";
+		
+		System.out.println("반지름이 5인 원의 둘레를 구하시오 : " + 2*PI*radius);
+		System.out.println(MY_NAME);
+
+	}
+
+}
---
+++ Example0831_2/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int smallNum = 5;
+		double bigNum = 18.99;
+		
+		bigNum = smallNum;
+		
+		System.out.println(bigNum);
+
+	}
+
+}
