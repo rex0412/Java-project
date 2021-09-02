847268c617419b7957c0302478e96160c20c69e6
DaeYong_0412 <rex041200@naver.com>
2021-09-02 21:29:55 +0900

    산술 연산자

--- Example0901_1/src/Main.java
+++ Example0901_1/src/Main.java
@@ -1,4 +1,10 @@
 
 public class Main {
 
+	public static void main(String[] args) {
+		int x = 0;
+		int y = 4;
+		
+		System.out.println(x + y);
+	}
 }
---
+++ Example0902_1/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		int firstNum = 10;
+		int secondNum = 5;
+		
+		System.out.println("firstNum + secondNum = " + (firstNum + secondNum));
+		System.out.println("firstNum - secondNum = " + (firstNum - secondNum));
+		System.out.println("firstNum * secondNum = " + (firstNum * secondNum));
+		System.out.println("firstNum / secondNum = " + (firstNum / secondNum));
+		System.out.println("firstNum % secondNum = " + (firstNum % secondNum));
+	}
+}
