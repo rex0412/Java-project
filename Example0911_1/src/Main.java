a8a6891f673fdc2a126c1cb8719e37526a865912
DaeYong_0412 <rex041200@naver.com>
2021-09-11 21:26:56 +0900

    제어문(반복문 - for문)

---
+++ Example0911_1/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Iterator;
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 8; i++) {
+			System.out.println("반복합니다.");
+		}
+	}
+}
---
+++ Example0911_2/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		for (int i = 0; i < 8; i++) {
+			System.out.println((i+1)+"번 반복합니다.");
+		}
+	}
+}
