7baca686c983728a87ae7ded265c2d71c00366ce
DaeYong_0412 <rex041200@naver.com>
2021-09-15 22:44:16 +0900

    멤버변수

---
+++ Example0915_1/src/Human.java
@@ -0,0 +1,16 @@
+
+public class Human {
+
+	String firstName;    //성
+	String secondName;   //이름
+	double height;       //키
+	double weight;       //몸무게
+	int heartRate;       //심박수
+	int car;             //자동차 수
+	boolean married;     //결혼 여부
+	String gender;
+
+	void heightMeasurement() {
+		double height;
+	}
+}
---
+++ Example0915_1/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		Human human = new Human();  //참조변수
+
+		human.firstName = "윤";
+		System.out.println(human.firstName);
+	}
+}
