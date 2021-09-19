b93e2a3f6d7eac7661d3b33a1eeb6ecd416a0a98
DaeYong_0412 <rex041200@naver.com>
2021-09-19 21:35:20 +0900

    this 예약어_1

---
+++ Example0919_1/src/ThisStudent.java
@@ -0,0 +1,16 @@
+
+public class ThisStudent {
+
+	int money;
+	String name;
+	boolean bordingCheck = false;
+	
+	public ThisStudent() {
+	}
+	
+	public ThisStudent(String name, int money) {
+		this.name = name;
+		this.money = money;
+	}
+
+}
