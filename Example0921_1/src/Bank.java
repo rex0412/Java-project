e2c08e1ee4c908cfb54bac3b2f72e3d5fad0f5d2
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-09-21 22:37:37 +0900

    Add files via upload

---
+++ Example0921_1/src/Bank.java
@@ -0,0 +1,14 @@
+
+public class Bank {
+	private int money;
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
+	}
+	
+
+}
---
+++ Example0921_1/src/BankMain.java
@@ -0,0 +1,15 @@
+
+public class BankMain {
+
+	public static void main(String[] args) {
+		
+		Bank pilsang = new Bank();
+		
+		pilsang.setMoney(1000000);
+		
+		System.out.println(pilsang.getMoney());
+		
+//		pilsang.money = 0;
+//		System.out.println(pilsang.money);
+	}
+}
