8986b1a19087e1dd1d125db7c7e001cc528aa533
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-10-08 22:47:32 +0900

    A+B -3

---
+++ Java Quiz A+B -3/src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+ 
+public class Main {
+	public static void main(String args[]) {
+ 
+		Scanner in = new Scanner(System.in);
+ 
+		int c = in.nextInt();
+		int arr[] = new int[c];
+ 
+		for (int i = 0; i < c; i++) {
+			int a = in.nextInt();
+			int b = in.nextInt();
+			arr[i] = a + b;
+		}
+		in.close();
+ 
+		for (int k : arr) {
+			System.out.println(k);
+		}
+	}
+ 
+}
