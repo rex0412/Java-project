f29e247d72e4f846b5edf9a75c33c448908bf739
DaeYong_0412 <84562268+rex0412@users.noreply.github.com>
2021-10-10 22:51:18 +0900

    피라미드 별찍기

--- Java Quiz 별찍기/src/Main.java
+++ Java Quiz 별찍기/src/Main.java
@@ -3,19 +3,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		for(int i = 1; i < 6; i++){
-			
-			for(int j = 5; j > 0; j--){
-				
-				if(i < j){
-					
-					    System.out.print(" ");
-				
-				}else{
-					    System.out.print("*");
-				}
+		for(int i=0;i<4;i++){
+			for(int j=0;j<3-i;j++){
+				System.out.print(" "); 
 			}
-			
+			for(int j=0;j<2*i+1;j++){
+				System.out.print("*"); 
+			}
 			System.out.println("");
 		}
 	}
